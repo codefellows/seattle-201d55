7a9f5609d2bbbfb7edbd7ba1cb73e157adb8d815
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-12-05 11:55:22 -0800

    finish day 4 demo

--- class-04/demo/app.js
+++ class-04/demo/app.js
@@ -1,1 +1,12 @@
+'use strict';
+
 console.log('linked!');
+
+// function myCoolNameForMyFunction() { actual lines of code that are part of that function }
+function greetings() {
+  console.log('hi!');
+  console.log('bye!');
+}
+
+// actually call the function! actually do the greetings!
+greetings();
