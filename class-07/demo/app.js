3e8f49b82fceb230f471ff3d35ed622397de66e7
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-12-10 12:03:31 -0800

    add day 7 demo

--- class-07/demo/app.js
+++ class-07/demo/app.js
@@ -5,39 +5,55 @@ console.log('js linked!');
 // Constructor!
 // Constructor names tend to be a singular noun
 // Take in values that we want as parameters
-function Pet(breedOfThisPet, weight) {
+function Pet(breedOfThisPet, weight, descriptionWords) {
   // Inside constructor, set up the properties we want for objects created with this constructor
   // set key/value pairs on the new object using contextual this
   this.breed = breedOfThisPet;
   // name of parameter and name of property can be the same or different
   this.weight = weight;
+  this.descriptionWords = descriptionWords;
 }
 
-var firstDog = {
-  weight: 35,
-  breed: 'Weimaraner',
-  imgUrl: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2016/05/19091354/Weimaraner-puppy-outdoors-with-bright-blue-eyes.20190813165758508-1.jpg',
-  descriptionWords: ['hungry', 'blue eyes', 'good boy', 'cute'],
-  getDescription: function() {
-    // get one of the description words at random about this dog
-    // contextual this: look inside the object on which the function was called to find the key
-    return this.descriptionWords[
-      Math.floor(Math.random() * this.descriptionWords.length)
-    ];
-  }
+// Pet.prototype.functionName = function(any, parameters, go, here)
+// Now, any pet created with the Pet constructor will be able to call this method
+// And by using contextual this, we can access the descriptionWords of that specific pet object
+Pet.prototype.getDescription = function() {
+  var randomIndex = Math.floor(Math.random() * this.descriptionWords.length);
+  return this.descriptionWords[randomIndex];
 };
 
-var firstCat = {
-  weight: 5,
-  breed: 'American shorthair',
-  descriptionWords: ['inquisitive', 'needy', 'small', 'whiskery'],
-  getDescription: function() {
-    return this.descriptionWords[Math.floor(Math.random() * this.descriptionWords.length)];
-  }
+// Add a prototype method to render a pet to the table
+// Because it's a prototype method, all pet instances will be able to call this method
+Pet.prototype.render = function() {
+  var animalTable = document.getElementById('animal-table');
+
+  // Create the tr and the td's to hold this pet's data
+  var petRow = document.createElement('tr');
+
+  // create breed td, add it to the row
+  var breedCell = document.createElement('td');
+  breedCell.textContent = this.breed;
+  petRow.appendChild(breedCell);
+
+  // create weight td, add it to the row
+  var weightCell = document.createElement('td');
+  weightCell.textContent = this.weight;
+  petRow.appendChild(weightCell);
+
+  // also add description
+  var descriptionCell = document.createElement('td');
+  descriptionCell.textContent = this.descriptionWords;
+  petRow.appendChild(descriptionCell);
+
+  // add to page by appending the new row of data to the table
+  animalTable.appendChild(petRow);
 };
 
+var firstDog = new Pet('Weimaraner', 35, ['hungry', 'blue eyes', 'good boy', 'cute']);
+var firstCat = new Pet('American shorthair', 5, ['inquisitive', 'needy', 'small', 'whiskery']);
+var lizard = new Pet('iguana', 8, ['green', 'scaly']);
 
-var pets = [firstDog, firstCat];
+var pets = [firstDog, firstCat, lizard];
 
 var animalHolder = document.getElementById('animal-holder');
 animalHolder.textContent = 'the animals go here';
@@ -48,9 +64,5 @@ animalHolder.textContent = 'the animals go here';
 
 // create a p tag inside of the animalHolder for each pet
 for (var i = 0; i < pets.length; i++) {
-  // first, ask the document to create an element for us
-  var newParagraph = document.createElement('p');
-  newParagraph.textContent = `An adoptable ${pets[i].breed} that is ${pets[i].getDescription()}`;
-  // then, append that element to the element on the page
-  animalHolder.appendChild(newParagraph);
+  pets[i].render();
 }
