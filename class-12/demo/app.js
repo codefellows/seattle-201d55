0cc07f1f359b7291718397c03852277db92ef79f
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-12-17 12:02:35 -0800

    day 12 demo with chartjs

--- class-12/demo/app.js
+++ class-12/demo/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 console.log('js linked!');
+// console.log(coolStuff);
 
 var imageElements = document.getElementsByTagName('img');
 var goatIndex1 = 0;
@@ -16,6 +17,13 @@ function Goat(name, imageUrl) {
   allGoats.push(this);
 }
 
+function getGoatArray(nameOfThePropertyIWant) {
+  var answer = [];
+  for (var i = 0; i < allGoats.length; i++) {
+    answer[i] = allGoats[i][nameOfThePropertyIWant];
+  }
+  return answer;
+}
 // actually create our goats
 
 new Goat('cruisin', 'images/cruisin-goat.jpg');
@@ -53,9 +61,46 @@ function imageWasClicked(event) {
 
   if(totalClicks >= 5) {
     // we made it to 5 clicks
-    var footerEl = document.getElementsByTagName('footer')[0];
-    // loop through all the goats and display how many times each one was picked
-    footerEl.textContent = `You picked things!`;
+
+    // chart code thanks to https://www.chartjs.org/docs/latest/
+    var ctx = document.getElementById('resultsChart').getContext('2d');
+    // eslint-disable-next-line no-undef
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        // what does labels do?
+        labels: getGoatArray('name'),
+        // what does datasets do?
+        // it's an array of objects
+        datasets: [{
+          // what does this label do?
+          // key, legend
+          label: '# of Votes',
+          // what does this data do?
+          // actually the values in the chart
+          data: getGoatArray('timesClicked'),
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              stepSize: 1
+            }
+          }]
+        }
+      }
+    });
   }
 }
 
